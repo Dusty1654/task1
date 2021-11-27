f2b28a3aed6bc46ec5d36371eac3dea024130db7
Dusty <vad.schadyanov1654@gmail.com>
2021-11-27 17:02:03 +0500

    Added Animal, which's extended by Cat and DogSerenityGay

--- src/org/example/test/DogSerenityGay.java
+++ src/org/example/test/DogSerenityGay.java
@@ -2,42 +2,16 @@ package org.example.test;
 
 import java.util.List;
 
-public class DogSerenityGay {
-    private String name;
-    private String owner;
+public class DogSerenityGay extends Animal {
+
     private float lifeTime;
-    private List<String> habits;
 
-    public DogSerenityGay(String name, String owner, float lifeTime, List<String> habits){
-        this.name = name;
-        this.owner = owner;
+    public DogSerenityGay(String name, String owner, float lifeTime, List<String> habits) {
+        super(name, owner, habits);
         this.lifeTime = lifeTime;
-        this.habits = habits;
-    }
-
-    @Override
-    public String toString() {
-        return "DogSerenityGay{" +
-                "name='" + name + '\'' +
-                ", owner='" + owner + '\'' +
-                ", lifeTime=" + lifeTime +
-                ", habits=" + habits +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getOwner() {
-        return owner;
     }
 
     public float getLifeTime() {
         return lifeTime;
-    }
-
-    public List<String> getHabits() {
-        return habits;
     }
 }
--- src/org/example/test/Main.java
+++ src/org/example/test/Main.java
@@ -5,14 +5,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Серенити", "Дасти",
-                new ArrayList<String>(){{
+                new ArrayList<String>() {{
                     add("Писать Дасти-Мордасти");
                 }});
-
-        System.out.println(cat.toString());
-
-        System.out.println(Calculator.sum(2,2));
-        System.out.println(Calculator.multiply(3.5f, 2));
-        System.out.println(Calculator.divide(16, 4));
     }
 }
