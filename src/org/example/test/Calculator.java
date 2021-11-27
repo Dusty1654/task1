8816e45c13966d33c9f41562c1631dc0c839d410
Dusty <vad.schadyanov1654@gmail.com>
2021-11-27 16:22:26 +0500

    Added realisation of Calculator methods

--- src/org/example/test/Calculator.java
+++ src/org/example/test/Calculator.java
@@ -1,4 +1,17 @@
 package org.example.test;
 
 public class Calculator {
+
+
+    public static float sum(float a, int b) {
+        return a + b;
+    }
+
+    public static float multiply(float a, int b) {
+        return a * b;
+    }
+
+    public static float divide(float a, int b) {
+        return a / b;
+    }
 }
